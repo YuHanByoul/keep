f188c158dc88af98fec3f37e665ba2b7e3bb2dd0
정승규 <jeongsk@kbrainc.com>
2022-10-19 10:28:06 +0900

    profile static 멤버변수 추가

--- src/main/java/com/kbrainc/plum/PlumApplication.java
+++ src/main/java/com/kbrainc/plum/PlumApplication.java
@@ -6,11 +6,14 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession; // REDIS_SESSION
 
@@ -25,7 +28,8 @@ import com.kbrainc.plum.rte.file.FileStorageProperties;
 @ConfigurationPropertiesScan({"com.kbrainc.plum.config.security.properties.${database.dialect}"})
 //@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7200) // REDIS_SESSION
 public class PlumApplication {
-
+    public static String profile;
+    
 	@Value("${app.timezone}")
     private String serverTimezone;
 	
@@ -39,6 +43,8 @@ public class PlumApplication {
 	}
 	
     public static void main(String[] args) {
+        ApplicationArguments applicationArguments = new DefaultApplicationArguments(args);
+        profile = applicationArguments.getOptionValues("spring.profiles.active").get(0).toString();
         SpringApplication.run(PlumApplication.class, args);
     }
 
