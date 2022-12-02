654de62c715dda6fd3bb028e91c74b07fdc9ca02
정승규 <jeongsk@kbrainc.com>
2022-12-02 18:18:36 +0900

    css 변경 내용 적용

--- src/main/resources/static/files/assets/pages/advance-elements/swithces.js
+++ src/main/resources/static/files/assets/pages/advance-elements/swithces.js
@@ -1,10 +1,9 @@
 "use strict";
 $(document).ready(function() {
     // Single swithces small (22/11/16 JM sm버전을 위해 추가함
-    var elemsingleSmall = document.querySelector('.js-single-small');
-    var switcherySmall = null;
-    if(elemsingleSmall != undefined){
-        switcherySmall = new Switchery(elemsingleSmall, { color: '#4680ff', jackColor: '#fff', size : 'small'});
+    var elemsingleSmallList = document.querySelectorAll('.js-single-small');
+    for(var i=0; i < elemsingleSmallList.length; i++){
+        new Switchery(elemsingleSmallList[i], { color: '#4680ff', jackColor: '#fff', size : 'small'});
     }
     
     
