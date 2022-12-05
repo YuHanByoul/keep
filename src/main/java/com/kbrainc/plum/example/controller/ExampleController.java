da3268d60dd62c3c900a160f8b987b445e37d1c5
정승규 <jeongsk@kbrainc.com>
2022-12-05 12:37:05 +0900

    예제 네비게이션 메뉴명 수정

--- src/main/java/com/kbrainc/plum/example/controller/ExampleController.java
+++ src/main/java/com/kbrainc/plum/example/controller/ExampleController.java
@@ -40,7 +40,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/index.html")
     public String index(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "예제 Index");
-        request.setAttribute("MENU_NAVI", "예제>Index");
+        request.setAttribute("MENU_NAVI", "예제 / Index");
         
         return "example/index";
     }
@@ -48,7 +48,7 @@ public class ExampleController {
     @RequestMapping(value = "example/validation.html")
     public String validation(HttpServletRequest request, Model model) {
         request.setAttribute("MENU_NAME", "폼유효성체크(관리자)");
-        request.setAttribute("MENU_NAVI", "예제>폼유효성체크(관리자)");
+        request.setAttribute("MENU_NAVI", "예제 / 폼유효성체크(관리자)");
         
         return "example/validation";
     }
@@ -56,7 +56,7 @@ public class ExampleController {
     @RequestMapping(value = "example/validation2.html")
     public String validation2(HttpServletRequest request, Model model) {
         request.setAttribute("MENU_NAME", "폼유효성체크(사용자)");
-        request.setAttribute("MENU_NAVI", "예제>폼유효성체크(사용자)");
+        request.setAttribute("MENU_NAVI", "예제 / 폼유효성체크(사용자)");
         
         return "example/validation2";
     }
@@ -64,7 +64,7 @@ public class ExampleController {
     @RequestMapping(value = "example/customTag.html")
     public String customTag(HttpServletRequest request, Model model) {
         request.setAttribute("MENU_NAME", "커스텀 태그(thymeleaf)");
-        request.setAttribute("MENU_NAVI", "예제>커스텀 태그(thymeleaf)");
+        request.setAttribute("MENU_NAVI", "예제 / 커스텀 태그(thymeleaf)");
 
         model.addAttribute("grpcd", "G0001");
         model.addAttribute("selectedCd", "A0002");
@@ -84,7 +84,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/submitPaging.html")
     public String syncPaging(PrgrmVo prgrmVo, HttpServletRequest request, Model model) throws Exception {
         request.setAttribute("MENU_NAME", "페이징(submit)");
-        request.setAttribute("MENU_NAVI", "예제>페이징(submit)");
+        request.setAttribute("MENU_NAVI", "예제 / 페이징(submit)");
         
         List<PrgrmVo> result = exampleService.selectPrgrmList(prgrmVo);
         model.addAttribute("list", result); // 페이징 데이터
@@ -93,8 +93,8 @@ public class ExampleController {
     
     @RequestMapping(value = "/example2/fileUpDown.html")
     public String fileUpDown(HttpServletRequest request) throws Exception {
-        request.setAttribute("MENU_NAME", "파일업로드/다운로드");
-        request.setAttribute("MENU_NAVI", "예제>파일업로드/다운로드");
+        request.setAttribute("MENU_NAME", "파일업로드 및 다운로드");
+        request.setAttribute("MENU_NAVI", "예제 / 파일업로드 및 다운로드");
         
         return "example/fileUpDown";
     }
@@ -102,23 +102,23 @@ public class ExampleController {
     @RequestMapping(value = "/example/editor.html")
     public String editor(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "에디터(CKEditor4)");
-        request.setAttribute("MENU_NAVI", "예제>에디터(CKEditor4)");
+        request.setAttribute("MENU_NAVI", "예제 / 에디터(CKEditor4)");
         
         return "example/ckeditor4";
     }
     
     @RequestMapping(value = "/example/grid.html")
     public String grid(HttpServletRequest request) throws Exception {
-        request.setAttribute("MENU_NAME", "에디터(jsGrid)");
-        request.setAttribute("MENU_NAVI", "예제>에디터(jsGrid)");
+        request.setAttribute("MENU_NAME", "그리드(jsGrid)");
+        request.setAttribute("MENU_NAVI", "예제 / 그리드(jsGrid)");
         
         return "example/jsgrid";
     }
     
     @RequestMapping(value = "/example/excel.html")
     public String excel(HttpServletRequest request) throws Exception {
-        request.setAttribute("MENU_NAME", "엑셀 일괄등록/다운로드");
-        request.setAttribute("MENU_NAVI", "예제>엑셀 일괄등록/다운로드");
+        request.setAttribute("MENU_NAME", "엑셀 일괄등록 및 다운로드");
+        request.setAttribute("MENU_NAVI", "예제 / 엑셀 일괄등록 및 다운로드");
         
         return "example/excel";
     }
@@ -126,7 +126,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/serverValidation.html")
     public String serverValidation(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "서버 validation");
-        request.setAttribute("MENU_NAVI", "예제>서버 validation");
+        request.setAttribute("MENU_NAVI", "예제 / 서버 validation");
         
         return "example/serverValidation";
     }
@@ -134,7 +134,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/sendMail.html")
     public String sendMail(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "메일발송");
-        request.setAttribute("MENU_NAVI", "예제>메일발송");
+        request.setAttribute("MENU_NAVI", "예제 / 메일발송");
         
         return "example/sendMail";
     }
@@ -142,7 +142,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/compress.html")
     public String compress(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "압축/압축해제");
-        request.setAttribute("MENU_NAVI", "예제>압축/압축해제");
+        request.setAttribute("MENU_NAVI", "예제>압축 / 압축해제");
         
         return "example/compress";
     }
@@ -150,7 +150,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/encrypt.html")
     public String encrypt(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "문자열 암호화");
-        request.setAttribute("MENU_NAVI", "예제>문자열 암호화");
+        request.setAttribute("MENU_NAVI", "예제 / 문자열 암호화");
         
         return "example/encrypt";
     }
@@ -158,7 +158,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/layout.html")
     public String layout(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "레이아웃(thymeleaf)");
-        request.setAttribute("MENU_NAVI", "예제>레이아웃(thymeleaf)");
+        request.setAttribute("MENU_NAVI", "예제 / 레이아웃(thymeleaf)");
         
         return "example/layout";
     }
@@ -166,7 +166,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/formSubmit.html")
     public String formSubmit(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "form submit");
-        request.setAttribute("MENU_NAVI", "예제>form submit");
+        request.setAttribute("MENU_NAVI", "예제 / form submit");
         
         return "example/formSubmit";
     }
@@ -174,7 +174,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/spinner.html")
     public String spinner(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "spinner");
-        request.setAttribute("MENU_NAVI", "예제>spinner");
+        request.setAttribute("MENU_NAVI", "예제 / spinner");
         
         return "example/spinner";
     }
@@ -182,7 +182,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/authUiUx.html")
     public String authUiUx(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "접근권한 View단 처리");
-        request.setAttribute("MENU_NAVI", "예제>접근권한 View단 처리");
+        request.setAttribute("MENU_NAVI", "예제 / 접근권한 View단 처리");
         
         return "example/authUiUx";
     }
@@ -190,7 +190,7 @@ public class ExampleController {
     @RequestMapping(value = "/example/code.html")
     public String code(HttpServletRequest request) throws Exception {
         request.setAttribute("MENU_NAME", "code 사용");
-        request.setAttribute("MENU_NAVI", "예제>code 사용");
+        request.setAttribute("MENU_NAVI", "예제 / code 사용");
         
         return "example/code";
     }
