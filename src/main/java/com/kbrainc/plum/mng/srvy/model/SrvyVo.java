972458169f23dcb78b8bad9bbeea7c6532e8c274
박희열 <phy@kbrainc.com>
2022-12-22 08:41:37 +0900

    설문관리 화면구성

---
+++ src/main/java/com/kbrainc/plum/mng/srvy/model/SrvyVo.java
@@ -0,0 +1,128 @@
+package com.kbrainc.plum.mng.srvy.model;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kbrainc.plum.rte.model.ParentRequestVo;
+import com.kbrainc.plum.rte.model.UserVo;
+
+import lombok.Data;
+
+/**
+ * 
+ * 설문Vo 클래스
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.srvy.model
+ * - SrvyVo.java
+ * </pre> 
+ *
+ * @ClassName : SrvyVo
+ * @Description : 설문Vo 클래스 
+ * @author : KBRAINC
+ * @date : 2022. 12. 21.
+ * @Version : 
+ * @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+ */
+@Data
+public class SrvyVo extends ParentRequestVo {
+    
+    /** 로그인 사용자 정보 */
+    private UserVo user;
+    
+    /** 설문 아이디 */
+    private int srvyid;
+    
+    /** 사이트 아이디 */
+    private int siteid;
+    
+    /** 설문지 아이디 */
+    private int qestnrid;
+    
+    /** 설문지명 */
+    private String qestnrNm;
+    
+    /** 문항수 */
+    private int qitemCnt;
+    
+    /** 대상자수 */
+    private int trprCnt;
+    
+    /** 설문명 */
+    @NotEmpty(message = "설문명을 입력해주세요.")
+    @Size(max = 20, message = "설문명은 20자 이하여야 합니다.")
+    private String srvyNm;
+    
+    /** 설명 */
+    @NotEmpty(message = "설문 설명을 입력해주세요.")
+    @Size(max = 400, message = "설문 설명은 400자 이하여야 합니다.")
+    private String expln;
+    
+    /** 설문기간 코드 */
+    private String srvyPrdCd;
+    
+    /** 시작 일자 */
+    private String bgngDe;
+    
+    /** 종료 일자 */
+    private String endDe;
+    
+    /** 이후 일수 */
+    private int aftrDaycnt;
+    
+    /** 이전 일수 */
+    private int bfrDaycnt;
+    
+    /** 사용여부 */
+    private String useYn;
+    
+    
+    /** 수정일시 */
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
+    private Date mdfcnDt;
+    
+    /** 수정자 아이디 */
+    private String mdfrid;
+    
+    /** 등록일시 */
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
+    private Date regDt;
+    
+    /** 등록자 아이디 */
+    private String rgtrid;
+    
+    /** 수정일자 정보 */
+    public Date getMdfcnDt() {
+        return mdfcnDt != null ? (Date) mdfcnDt.clone() : null;
+    }
+    
+    public void setMdfcnDt(Date mdfcnDt) {
+        this.mdfcnDt = mdfcnDt != null ? (Date) mdfcnDt.clone() : null;
+    }
+    
+    /** 등록일자 정보 */
+    public Date getRegDt() {
+        return regDt != null ? (Date) regDt.clone() : null;
+    }
+    
+    public void setRegDt(Date regDt) {
+        this.regDt = regDt != null ? (Date) regDt.clone() : null;
+    }
+    
+    /** 로그인 사용자 정보 */
+    public UserVo getUser() {
+        UserVo clone = (UserVo) SerializationUtils.clone(this.user);
+        return  clone;
+    }
+    
+    public void setUser(UserVo user) {
+        UserVo clone = (UserVo) SerializationUtils.clone(user);
+        this.user = clone;
+    }
+    
+}
---
+++ src/main/java/com/kbrainc/plum/mng/srvy/service/SrvyService.java
@@ -0,0 +1,146 @@
+package com.kbrainc.plum.mng.srvy.service;
+
+import java.util.List;
+
+import com.kbrainc.plum.mng.srvy.model.SrvyVo;
+
+/**
+ * 
+ * 서비스 인터페이스
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.qestnr.service
+ * - QestnrService.java
+ * </pre> 
+ *
+ * @ClassName : SrvyService
+ * @Description : 설문관리 서비스 인터페이스 
+ * @author : KBRAINC
+ * @date : 2022. 12. 21.
+ * @Version : 
+ * @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+ */
+public interface SrvyService {
+    
+    /**
+     * 설문지 정보 등록
+     *
+     * @Title : insertQestnr 
+     * @Description : 설문지 정보 등록
+     * @param qestnrVo QestnrVo객체
+     * @return int qestrnid
+     * @throws Exception 예외
+     */
+    //public int insertQestnr(QestnrVo qestnrVo) throws Exception;
+    
+    /**
+     * 대상자설문 목록 조회
+     *
+     * @Title : selectTrprSrvyList 
+     * @Description : 대상자설문 목록 조회
+     * @param srvyVo SrvyVo객체
+     * @return List<SrvyVo> 설문 목록
+     * @throws Exception 예외
+     */
+    public List<SrvyVo> selectTrprSrvyList(SrvyVo srvyVo) throws Exception;
+     
+    /**
+     * 설문지 정보 조회
+     *
+     * @Title : selectQestnrInfo
+     * @Description : 설문지 정보 조회
+     * @param qestnrVo QestnrVo 객체
+     * @return QestnrVo QestnrVo 객체
+     * @throws Exception 예외
+     */
+    //public QestnrVo selectQestnrInfo(QestnrVo qestnrVo) throws Exception;
+     
+    /**
+     * 설문지 정보 업데이트
+     *
+     * @Title : updateQestnr
+     * @Description : 설문지 정보 업데이트
+     * @param qestnrVo QestnrVo 객체
+     * @return int update 로우수
+     * @throws Exception 예외
+     */
+    //public int updateQestnr(QestnrVo qestnrVo) throws Exception;
+     
+    /**
+     * 설문지 문항 목록 조회
+     *
+     * @Title : selectQitemList
+     * @Description : 설문지 목록 조회
+     * @param qitemVo QitemVo 객체
+     * @return List<QitemVo> 설문지 문항 목록
+     * @throws Exception 예외
+     */
+    //public List<QitemVo> selectQitemList(QitemVo qitemVo) throws Exception;
+      
+    /**
+     * 설문지 문항 등록
+     *
+     * @Title : insertQitem 
+     * @Description : 설문지 문항 등록
+     * @param qitemVo QitemVo객체
+     * @return int insert 로우수
+     * @throws Exception 예외
+     */
+    //public int insertQitem(QitemVo qitemVo) throws Exception;
+    
+    /**
+     * 설문지 문항 정보 조회
+     *
+     * @Title : selectQitemInfo
+     * @Description : 설문지 문항 정보 조회
+     * @param qitemVo QitemVo 객체
+     * @return QitemVo QitemVo 객체
+     * @throws Exception 예외
+     */
+    //public QitemVo selectQitemInfo(QitemVo qitemVo) throws Exception;
+    
+    /**
+     * 설문지 문항 보기 목록 조회
+     *
+     * @Title : selectQitemExList
+     * @Description : 설문지 문항 보기 목록 조회
+     * @param qitemVo QitemVo 객체
+     * @return List<QitemExVo> 설문지 문항 보기 목록
+     * @throws Exception 예외
+     */
+    //public List<QitemExVo> selectQitemExList(QitemVo qitemVo) throws Exception;
+    
+    /**
+     * 설문지 문항 정보 업데이트
+     *
+     * @Title : updateQitem
+     * @Description : 설문지 문항 정보 업데이트
+     * @param qitemVo QitemVo 객체
+     * @return int update 로우수
+     * @throws Exception 예외
+     */
+    //public int updateQitem(QitemVo qitemVo) throws Exception;
+    
+    /**
+     * 설문지 문항 순서 업데이트
+     *
+     * @Title : updateQitemOrdr
+     * @Description : 설문지 문항 순서 업데이트
+     * @param qitemVo QitemVo 객체
+     * @return int update 로우수
+     * @throws Exception 예외
+     */
+    //public int updateQitemOrdr(QitemVo qitemVo) throws Exception;
+    
+    /**
+     * 설문지 문항 삭제
+     *
+     * @Title : deleteQitem
+     * @Description : 설문지 문항 삭제
+     * @param qitemVo QitemVo 객체
+     * @return int delete 로우수
+     * @throws Exception 예외
+     */
+    //public int deleteQitem(QitemVo qitemVo) throws Exception;
+    
+}
