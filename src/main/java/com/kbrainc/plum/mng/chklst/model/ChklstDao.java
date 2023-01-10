65b55076359c6d89b1c389a907c59607c350bbf4
박희열 <phy@kbrainc.com>
2023-01-10 09:51:41 +0900

    설문관리, 커뮤니티관리, 체크리스트관리 검색 키워드 스타일 변경 적용

--- src/main/java/com/kbrainc/plum/mng/chklst/model/ChklstDao.java
+++ src/main/java/com/kbrainc/plum/mng/chklst/model/ChklstDao.java
@@ -81,124 +81,47 @@ public interface ChklstDao {
     public int updateChklstQitem(ChklstQitemVo chklstQitemVo) throws Exception;
     
     /**
-     * 설문지 문항 조회
+     * 체크리스트 등록
      *
-     * @Title : selectQitemList
-     * @Description : 설문지 목록 조회
-     * @param qestnrVo QestnrVo 객체
-     * @return List<QitemVo> 설문지 목록
-     * @throws Exception 예외
-      */
-    //public List<QitemVo> selectQitemList(QitemVo qitemVo) throws Exception;
-     
-    /**
-     * 설문지 문항 정보 등록
-     *
-     * @Title : insertQitem 
-     * @Description : 설문지 문항 정보 등록
-     * @param qitemVo QitemVo객체
+     * @Title : insertChklst 
+     * @Description : 체크리스트 등록
+     * @param chklstVo ChklstVo객체
      * @return int insert 로우수
      * @throws Exception 예외
      */
-    //public int insertQitem(QitemVo qitemVo) throws Exception;
+    public int insertChklst(ChklstVo chklstVo) throws Exception;
     
     /**
-     * 설문지 문항 보기 정보 등록
+     * 체크리스트 목록 조회
      *
-     * @Title : insertQitemEx 
-     * @Description : 설문지 문항 보기 정보 등록
-     * @param qitemExVo List<QitemExVo>객체
-     * @return int insert 로우수
+     * @Title : selectChklstList
+     * @Description : 체크리스트 문항 조회
+     * @param chklstVo  chklstVo 객체
+     * @return List<ChklstVo> 체크리스트 문항 목록
      * @throws Exception 예외
      */
-    //public int insertQitemEx(QitemExVo qitemExVo) throws Exception;
-
-    /**
-     * 설문지 문항 정보 조회
-     *
-     * @Title : selectQitemInfo
-     * @Description : 설문지 문항 정보 조회
-     * @param qitemVo QitemVo 객체
-     * @return QitemVo QitemVo 객체
-     * @throws Exception 예외
-     */
-    //public QitemVo selectQitemInfo(QitemVo qitemVo) throws Exception;
+    public List<ChklstVo> selectChklstList(ChklstVo chklstVo) throws Exception;
     
     /**
-     * 설문지 문항 보기 목록 조회
+     * 체크리스트 정보 조회
      *
-     * @Title : selectQitemExList
-     * @Description : 설문지 문항 보기 목록 조회
-     * @param qitemVo QitemVo 객체
-     * @return List<QitemExVo> 설문지 문항 보기 목록
+     * @Title : selectChklstInfo
+     * @Description : 체크리스트 정보 조회
+     * @param chklstVo ChklstVo 객체
+     * @return ChklstVo ChklstVo 객체
      * @throws Exception 예외
      */
-    //public List<QitemExVo> selectQitemExList(QitemVo qitemVo) throws Exception;
+    public ChklstVo selectChklstInfo(ChklstVo chklstVo) throws Exception;
     
     /**
-     * 설문지 문항 정보 업데이트
+     * 체크리스트 업데이트
      *
-     * @Title : updateQitem
-     * @Description : 설문지 문항 정보 업데이트
-     * @param qitemVo qitemVo 객체
+     * @Title : updateChklst
+     * @Description : updateChklst 업데이트
+     * @param chklstVo ChklstVo 객체
      * @return int update 로우수
      * @throws Exception 예외
      */
-    //public int updateQitem(QitemVo qitemVo) throws Exception;
-    
-    /**
-     * 설문지 다음 문항 정보 조회
-     *
-     * @Title : selectNextQitemInfo
-     * @Description : 설문지 다음 문항 정보 조회
-     * @param qitemVo QitemVo 객체
-     * @return QitemVo QitemVo 객체
-     * @throws Exception 예외
-     */
-    //public QitemVo selectNextQitemInfo(QitemVo qitemVo) throws Exception;
-    
-    /**
-     * 설문지 이전 문항 정보 조회
-     *
-     * @Title : selectNextQitemInfo
-     * @Description : 설문지 이전 문항 정보 조회
-     * @param qitemVo QitemVo 객체
-     * @return QitemVo QitemVo 객체
-     * @throws Exception 예외
-     */
-    //public QitemVo selectPrevQitemInfo(QitemVo qitemVo) throws Exception;
-    
-    /**
-     * 설문지 문항 순서 업데이트
-     *
-     * @Title : updateQitemOrdr
-     * @Description : 설문지 문항 순서 업데이트
-     * @param qitemVo QitemVo 객체
-     * @return int update 로우수
-     * @throws Exception 예외
-     */
-    //public int updateQitemOrdr(QitemVo qitemVo) throws Exception;
-    
-    /**
-     * 설문지 문항 삭제
-     *
-     * @Title : deleteQitem
-     * @Description : 설문지 문항 삭제
-     * @param qitemVo QitemVo 객체
-     * @return int delete 로우수
-     * @throws Exception 예외
-     */
-    //public int deleteQitem(QitemVo qitemVo) throws Exception;
-    
-    /**
-     * 설문지 문항 보기 삭제
-     *
-     * @Title : deleteQitemEx
-     * @Description : 설문지 문항 보기 삭제
-     * @param qitemVo QitemVo 객체
-     * @return int delete 로우수
-     * @throws Exception 예외
-     */
-    //public int deleteQitemEx(QitemVo qitemVo) throws Exception;
+    public int updateChklst(ChklstVo chklstVo) throws Exception;
     
 }
--- src/main/java/com/kbrainc/plum/mng/chklst/service/ChklstServiceImpl.java
+++ src/main/java/com/kbrainc/plum/mng/chklst/service/ChklstServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.kbrainc.plum.mng.chklst.model.ChklstDao;
 import com.kbrainc.plum.mng.chklst.model.ChklstQitemVo;
+import com.kbrainc.plum.mng.chklst.model.ChklstVo;
 import com.kbrainc.plum.mng.code.model.CodeVo;
 import com.kbrainc.plum.rte.service.PlumAbstractServiceImpl;
 
@@ -107,203 +108,67 @@ public class ChklstServiceImpl extends PlumAbstractServiceImpl implements Chklst
              
         return retVal;
     }
-     
-     /**
-      * 설문지 문항 목록 조회
-      *
-      * @Title : selectQitemList
-      * @Description : 설문지 목록 조회
-      * @param QitemVo qitemVo 객체
-      * @return List<QitemVo> 설문지 문항 목록
-      * @throws Exception 예외
-      */
-//     @Override
-//     public List<QitemVo> selectQitemList(QitemVo qitemVo) throws Exception {
-//         return qestnrDao.selectQitemList(qitemVo);
-//     }
     
     /**
-    * 설문지 문항 정보 등록
+     * 체크리스트 등록
      *
-     * @Title : insertQitem 
-     * @Description : 설문지 문항 정보 등록
-     * @param qitemVo QitemVo
+     * @Title : insertChklst 
+     * @Description : 체크리스트 등록
+     * @param chklstVo ChklstVo
      * @return int insert 로우수
      * @throws Exception 예외
      */
-//    @Override
-//    @Transactional
-//    public int insertQitem(QitemVo qitemVo) throws Exception {
-//        int retVal = 0;
-//        retVal = qestnrDao.insertQitem(qitemVo);
-//        List<QitemExVo> exampleList = qitemVo.getExampleList();
-//        if(exampleList != null && exampleList.size() > 0) {
-//            QitemExVo qitemExVo = null;
-//            for(int i = 0 ; i < exampleList.size() ; i++) {
-//                qitemExVo = exampleList.get(i);
-//                qitemExVo.setUser(qitemVo.getUser());
-//                qitemExVo.setQitemid(qitemVo.getQitemid());
-//                retVal = qestnrDao.insertQitemEx(qitemExVo);
-//            }
-//        }
-//        
-//        return retVal;
-//    }
-    
-    /**
-     * 설문지 문항 정보 조회
-     *
-     * @Title : selectQitemInfo
-     * @Description : 설문지 문항 정보 조회
-     * @param qitemVo QitemVo 객체
-     * @return QitemVo QitemVo 객체
-     * @throws Exception 예외
-     */
-//    @Override
-//    public QitemVo selectQitemInfo(QitemVo qitemVo) throws Exception {
-//        return qestnrDao.selectQitemInfo(qitemVo);
-//    }
-    
-    /**
-     * 설문지 문항 보기 목록 조회
-     *
-     * @Title : selectQitemExList
-     * @Description : 설문지 문항 보기 목록 조회
-     * @param QitemVo qitemVo 객체
-     * @return List<QitemVo> 설문지 문항 목록
-     * @throws Exception 예외
-     */
-//    @Override
-//    public List<QitemExVo> selectQitemExList(QitemVo qitemVo) throws Exception {
-//        return qestnrDao.selectQitemExList(qitemVo);
-//    }
-    
-    /**
-     * 설문지 문항 정보 업데이트
-     *
-     * @Title : updateQitem
-     * @Description : 설문지 정보 업데이트
-     * @param qitemVo QitemVo 객체
-     * @return int update 로우수
-     * @throws Exception 예외
-     */
-//    @Override
-//    @Transactional
-//    public int updateQitem(QitemVo qitemVo) throws Exception {
-//        int retVal = 0;
-//        // 1. 이전 등록된 보기 삭제
-//        String[] deleteIds = new String[1];
-//        deleteIds[0] = String.valueOf(qitemVo.getQitemid());
-//        qitemVo.setDeleteQitemids(deleteIds);
-//        retVal = qestnrDao.deleteQitemEx(qitemVo);
-//        // 2. 문항 정보 업데이트
-//        retVal = qestnrDao.updateQitem(qitemVo);
-//        // 3. 보기 등록
-//        List<QitemExVo> exampleList = qitemVo.getExampleList();
-//        if(exampleList != null && exampleList.size() > 0) {
-//            QitemExVo qitemExVo = null;
-//            for(int i = 0 ; i < exampleList.size() ; i++) {
-//                qitemExVo = exampleList.get(i);
-//                qitemExVo.setUser(qitemVo.getUser());
-//                qitemExVo.setQitemid(qitemVo.getQitemid());
-//                retVal = qestnrDao.insertQitemEx(qitemExVo);
-//            }
-//        }
-//             
-//        return retVal;
-//    }
-    
-    /**
-     * 설문지 문항 순서 업데이트
-     *
-     * @Title : updateQestnr
-     * @Description : 설문지 정보 업데이트
-     * @param qitemVo QitemVo 객체
-     * @return int update 로우수
-     * @throws Exception 예외
-     */
-//    @Override
-//    @Transactional
-//    public int updateQitemOrdr(QitemVo qitemVo) throws Exception {
-//        int retVal = 0;
-//        int ordr = qitemVo.getOrdr();
-//        String dir = qitemVo.getChangeDir();
-//        // 1. 다음(이전) 문항 조회
-//        QitemVo changeQitemVo = null;
-//        if(dir.equals("next")) { // 다음 문항
-//            changeQitemVo = qestnrDao.selectNextQitemInfo(qitemVo);
-//            qitemVo.setOrdr(ordr + 1);
-//            changeQitemVo.setOrdr(changeQitemVo.getOrdr() - 1);
-//        } else { // 이전 문항
-//            changeQitemVo = qestnrDao.selectPrevQitemInfo(qitemVo);
-//            qitemVo.setOrdr(ordr - 1);
-//            changeQitemVo.setOrdr(changeQitemVo.getOrdr() + 1);
-//        }
-//        // 2. 선택 문항 업데이트
-//        retVal = qestnrDao.updateQitemOrdr(qitemVo);
-//        // 3. 다음(이전) 문항 업데이트
-//        retVal = qestnrDao.updateQitemOrdr(changeQitemVo);
-//             
-//        return retVal;
-//    }
-    
-    /**
-     * 설문지 문항 삭제
-     *
-     * @Title : updateQestnr
-     * @Description : 설문지 정보 업데이트
-     * @param qitemVo QitemVo 객체
-     * @return int delete 로우수
-     * @throws Exception 예외
-     */
-//    @Override
-//    @Transactional
-//    public int deleteQitem(QitemVo qitemVo) throws Exception {
-//        int retVal = 0;
-//        
-//        // 1. 문항에 걸려있는 보기 삭제
-//        retVal = qestnrDao.deleteQitemEx(qitemVo);
-//        // 2. 문항 삭제
-//        retVal = qestnrDao.deleteQitem(qitemVo);
-//        // 3. 문항 조회 후 순서 업데이트
-//        qitemVo.setOrderField("ORDR");
-//        qitemVo.setOrderDirection(ORDER_DIRECTION.asc);
-//        List<QitemVo> qitemList = qestnrDao.selectQitemList(qitemVo);
-//        if(qitemList != null && qitemList.size() > 0) {
-//            for(int i = 0; i < qitemList.size(); i++) {
-//                QitemVo qitem = qitemList.get(i);
-//                qitem.setOrdr(i + 1);
-//                retVal = qestnrDao.updateQitemOrdr(qitem);
-//            }
-//        }
-//        
-//        return retVal;
-//    }
-    
-    /**
-     * 설문지 문항, 보기 목록 조회
-     *
-     * @Title : selectQitemWithExList
-     * @Description : 설문지 문항, 보기 목록 조회
-     * @param QitemVo qitemVo 객체
-     * @return List<QitemVo> 설문지 문항, 보기 목록
-     * @throws Exception 예외
-     */
-//    @Override
-//    public List<QitemVo> selectQitemWithExList(QitemVo qitemVo) throws Exception {
-//        List<QitemVo> qitemList = qestnrDao.selectQitemList(qitemVo);
-//        if(qitemList != null && qitemList.size() > 0) {
-//            for(int i = 0; i < qitemList.size(); i++) {
-//                QitemVo qitem = qitemList.get(i);
-//                int exCnt = qitem.getExCnt();
-//                if(exCnt > 0) {
-//                    List<QitemExVo> qitemExList = qestnrDao.selectQitemExList(qitem);
-//                    qitem.setExampleList(qitemExList);
-//                }
-//            }
-//        }
-//        
-//        return qitemList;
-//    }
+     @Override
+     public int insertChklst(ChklstVo chklstVo) throws Exception {
+         int retVal = 0;
+         retVal += chklstDao.insertChklst(chklstVo);
+         
+         return retVal;
+     }
      
+     /**
+      * 체크리스트 목록 조회
+      *
+      * @Title : selectChklstList
+      * @Description : 체크리스트 문항 구분코드 목록 조회
+      * @param chklstVo ChklstVo 객체
+      * @return List<ChklstVo> 체크리스트 문항 목록
+      * @throws Exception 예외
+      */
+     @Override
+     public List<ChklstVo> selectChklstList(ChklstVo chklstVo) throws Exception {
+         return chklstDao.selectChklstList(chklstVo);
+     }
+     
+     /**
+      * 체크리스트 정보 조회
+      *
+      * @Title : selectChklstQitemInfo
+      * @Description : 체크리스트 정보 조회
+      * @param chklstVo ChklstVo 객체
+      * @return ChklstVo ChklstVo 객체
+      * @throws Exception 예외
+      */
+     @Override
+     public ChklstVo selectChklstInfo(ChklstVo chklstVo) throws Exception {
+         return chklstDao.selectChklstInfo(chklstVo);
+     }
+     
+     /**
+      * 체크리스트 업데이트
+      *
+      * @Title : updateChklst
+      * @Description : 체크리스트 업데이트
+      * @param chklstVo ChklstVo 객체
+      * @return int update 로우수
+      * @throws Exception 예외
+      */
+     @Override
+     public int updateChklst(ChklstVo chklstVo) throws Exception {
+         int retVal = 0;
+         retVal = chklstDao.updateChklst(chklstVo);        
+              
+         return retVal;
+     }
+    
 }
