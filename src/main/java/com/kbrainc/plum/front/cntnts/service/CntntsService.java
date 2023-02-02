26b6328589fcd41364e9897697ba92dbe2475fe2
서정도 <sjd4328@kbrainc.com>
2023-02-02 18:08:55 +0900

    사용자 콘텐츠 중간 커밋

---
+++ src/main/java/com/kbrainc/plum/front/cntnts/service/CntntsService.java
@@ -0,0 +1,36 @@
+package com.kbrainc.plum.front.cntnts.service;
+
+import java.util.List;
+
+import com.kbrainc.plum.front.cntnts.model.CntntsVo;
+
+
+
+/**
+* 컨텐츠 관리 서비스 클래스
+*
+* <pre>
+* com.kbrainc.plum.mng.cntnts.service
+* - CntntsService.java
+* </pre>
+*
+* @ClassName : CntntsService
+* @Description : 컨텐츠 관리 서비스 클래스
+* @author : JD
+* @date : 2023. 1. 12.
+* @Version :
+* @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+*/
+public interface CntntsService {
+    
+    /**
+    * 컨텐츠 관리 게시글 목록 조회
+    *
+    * @Title : selectCntntsList
+    * @Description : 컨텐츠 관리 게시글 목록 조회
+    * @param cntntsVo 객체
+    * @throws Exception 예외
+    * @return List<CntntsVo>
+    */
+    public List<CntntsVo> selectCntntsList(CntntsVo cntntsVo) throws Exception;
+}
