0f61c2da68699ca064145b6ad812413ca35be77f
정승규 <jeongsk@kbrainc.com>
2023-02-23 09:59:17 +0900

    사이트 변경 필터 수정

--- src/main/java/com/kbrainc/plum/rte/filter/SiteChangeFilter.java
+++ src/main/java/com/kbrainc/plum/rte/filter/SiteChangeFilter.java
@@ -81,7 +81,7 @@ public class SiteChangeFilter implements Filter {
                     
                     UserVo user = (UserVo) session.getAttribute("user");
                     if (user != null) { // 로그인 되어있는 상태에서 도메인 변경시
-                        // 역할을 변경한다.(포털인데 기관사용자역할이 있으면 해당역할로 변경 구현 필요!!) 
+                        // 역할을 변경한다.
                         for (Map<String, String> authority : user.getAuthorities()) {
                             
                             if ("P".equals(siteInfo.getSysKndCd()) && "I".equals(user.getLoginUserType())) { // 사용자포털사이트 & 기관회원역할
@@ -89,7 +89,7 @@ public class SiteChangeFilter implements Filter {
                                 authorities.add(new SimpleGrantedAuthority(sysCompanyRoleid));
                                 authorities.add(new SimpleGrantedAuthority("0")); // anonymous권한 기본 부여
                                 SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null, authorities));
-                                RoleInfoVo roleInfo = new RoleInfoVo((String)authority.get("roleid"), (String)authority.get("nm"), (String)authority.get("knd_cd"), (String)authority.get("se_cd"), (String)authority.get("trgt_inst_cd"), (String)authority.get("trgt_rgn_cd"));
+                                RoleInfoVo roleInfo = new RoleInfoVo(sysCompanyRoleid, "기관회원", "O", "U", null, null);
                                 user.setRoleInfo(roleInfo);
                                 break;
                             } else if ("U".equals(siteInfo.getSysSeCd()) && "U".equals(authority.get("se_cd"))) { // 사용자사이트 & 사용자역할
@@ -98,7 +98,7 @@ public class SiteChangeFilter implements Filter {
                                     authorities.add(new SimpleGrantedAuthority(authority.get("roleid")));
                                     authorities.add(new SimpleGrantedAuthority("0")); // anonymous권한 기본 부여
                                     SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null, authorities));
-                                    RoleInfoVo roleInfo = new RoleInfoVo((String)authority.get("roleid"), (String)authority.get("nm"), (String)authority.get("knd_cd"), (String)authority.get("se_cd"), (String)authority.get("trgt_inst_cd"), (String)authority.get("trgt_rgn_cd"));
+                                    RoleInfoVo roleInfo = new RoleInfoVo((String)authority.get("roleid"), (String)authority.get("nm"), (String)authority.get("knd_cd"), (String)authority.get("se_cd"), null, null);
                                     user.setRoleInfo(roleInfo);
                                     break;
                                 }
