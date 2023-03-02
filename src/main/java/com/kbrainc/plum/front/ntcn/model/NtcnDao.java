68703acfc14f513b0842c870781baa032e1a4a4d
송진규 <supersjk0910@gamil.com>
2023-03-02 14:42:08 +0900

    알림(공통)

---
+++ src/main/java/com/kbrainc/plum/front/ntcn/model/NtcnDao.java
@@ -0,0 +1,36 @@
+package com.kbrainc.plum.front.ntcn.model;
+
+import java.util.List;
+
+import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
+
+/**
+ * 
+ * 알림 사용자 DAO 인터페이스
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.pop.model
+ * - NtcnDao.java
+ * </pre> 
+ *
+ * @ClassName : NtcnDao
+ * @Description : 알림 사용자 DAO
+ * @author : KBRAINC
+ * @date : 2021. 2. 27.
+ * @Version : 
+ * @Company : Copyright KBRAIN Company. All Rights Reserved
+ */
+@Mapper("front.ntcnDao")
+public interface NtcnDao {
+    
+    /**
+     * @Title : selectMainNtcnList
+     * @Description : 메인화면 알림리스트(5개) 호출 
+     * @param NtcnVo
+     * @throws Exception
+     * @return List<NtcnVo> 객체
+     */
+    public List<NtcnVo> selectMainNtcnList(NtcnVo NtcnVo) throws Exception;
+    
+
+}
---
+++ src/main/java/com/kbrainc/plum/front/ntcn/service/NtcnServiceImpl.java
@@ -0,0 +1,47 @@
+package com.kbrainc.plum.front.ntcn.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.type.Alias;
+import org.springframework.stereotype.Service;
+
+import com.kbrainc.plum.front.ntcn.model.NtcnDao;
+import com.kbrainc.plum.front.ntcn.model.NtcnVo;
+import com.kbrainc.plum.rte.service.PlumAbstractServiceImpl;
+
+/**
+ * 
+ * PopServiceImpl
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.pop.service
+ * - PopServiceImpl.java
+ * </pre> 
+ *
+ * @ClassName : PopServiceImpl
+ * @Description : PopServiceImpl
+ * @author : KBRAINC
+ * @date : 2021. 2. 26.
+ * @Version : 
+ * @Company : Copyright KBRAIN Company. All Rights Reserved
+ */
+@Service("front.ntcnServiceImpl")
+@Alias("front.ntcnServiceImpl")
+public class NtcnServiceImpl extends PlumAbstractServiceImpl implements NtcnService {
+
+	@Resource(name = "front.ntcnDao")
+	NtcnDao ntcnDao;
+	
+    /**
+     * @Title : selectMainNtcnList
+     * @Description : 메인화면 알림리스트(5개) 호출 
+     * @param NtcnVo
+     * @throws Exception
+     * @return List<NtcnVo> 객체
+     */
+    public List<NtcnVo> selectMainNtcnList(NtcnVo NtcnVo) throws Exception{
+        return ntcnDao.selectMainNtcnList(NtcnVo);
+    }
+}
