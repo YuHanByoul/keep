b47b4cad67cdd417e2778e0a1737d3004458634a
lgc <lgc@kbrainc.com>
2023-03-03 17:50:50 +0900

    내주변 환경교육시설 지도 개발 중....

--- src/main/java/com/kbrainc/plum/cmm/service/CommonServiceImpl.java
+++ src/main/java/com/kbrainc/plum/cmm/service/CommonServiceImpl.java
@@ -192,8 +192,8 @@ public class CommonServiceImpl extends PlumAbstractServiceImpl implements Common
      * @return 내 주변 환경교육 시설 목록
      * @throws Exception 예외
      */
-    public List<Map<String, Object>> nearbyEnveduFlct() throws Exception {
-        return commonDao.nearbyEnveduFlct();
+    public List<Map<String, Object>> nearbyEnveduFlct(Map map) throws Exception {
+        return commonDao.nearbyEnveduFlct(map);
     }
     
     
