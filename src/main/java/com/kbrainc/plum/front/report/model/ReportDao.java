3b807691ec6c01440f242a1dd6b0fe441adddb71
changsu <changsu@192.168.0.128>
2023-03-15 17:55:31 +0900

    포탈 > 마이페이지 > 체험환경 > 사업보고관리

---
+++ src/main/java/com/kbrainc/plum/front/report/model/ReportDao.java
@@ -0,0 +1,150 @@
+/**
+ * 
+ */
+package com.kbrainc.plum.front.report.model;
+
+import java.util.List;
+
+import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
+
+import com.kbrainc.plum.front.bizAply.model.SupplementVo;
+
+/**
+* [클래스 요약]. 
+*
+* <pre>
+* com.kbrainc.plum.front.report.model
+* - ReportDao.java
+* </pre> 
+*
+* @ClassName : ReportDao
+* @Description : TODO
+* @author : KCS
+* @date : 2023. 3. 13.
+* @Version :
+* @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+*/
+@Mapper
+public interface ReportDao {
+
+    /**
+    * 사업보고관리 목록 조회. 
+    *
+    * @Title : selectDelvryAplyList
+    * @Description : TODO
+    * @param reportVo
+    * @return
+    * @throws Exception
+    * @return List<ReportVo>
+     */
+    List<ReportVo> selectReportList(ReportVo reportVo) throws Exception;
+     
+    /**
+     * 
+    * 사업보고관리 등록. 
+    *
+    * @Title : insertReport
+    * @Description : TODO
+    * @param reportVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int insertReport(ReportVo reportVo) throws Exception;
+
+    /**
+    * 사업보고관리 수정. 
+    *
+    * @Title : updateReport
+    * @Description : TODO
+    * @param reportVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int updateReport(ReportVo reportVo) throws Exception;
+
+    /**
+    * 사업보고관리 교육운영정보 목록 조회. 
+    *
+    * @Title : selectReportOperList
+    * @Description : TODO
+    * @param reportVo
+    * @return
+    * @throws Exception
+    * @return List<ReportOperVo>
+     */
+    List<ReportOperVo> selectReportOperList(ReportOperVo reportVo) throws Exception;
+    
+    /**
+    * 사업보고관리 교육운영정보 등록. 
+    *
+    * @Title : insertReportOper
+    * @Description : TODO
+    * @param reportOperVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int insertReportOper(ReportOperVo reportOperVo) throws Exception;
+    
+    /**
+    * 사업보고관리 교육운영정보 수정.
+    *
+    * @Title : updateReportOper
+    * @Description : TODO
+    * @param reportOperVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int updateReportOper(ReportOperVo reportOperVo) throws Exception;
+    
+    /**
+    * 보완요청 상세조회 
+    *
+    * @Title : selectSplmntInfo
+    * @Description : TODO
+    * @param supplementVo
+    * @return
+    * @throws Exception
+    * @return SupplementVo
+     */
+    SupplementVo selectSplmntInfo(SupplementVo supplementVo) throws Exception;
+    
+    /**
+    * 보완요청 답변 수정. 
+    *
+    * @Title : updateSplmnt
+    * @Description : TODO
+    * @param supplementVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int updateSplmnt(SupplementVo supplementVo) throws Exception;
+
+    /**
+    * 중간보고관리 보완완료 상태 업데이트. 
+    *
+    * @Title : updateReportStts
+    * @Description : TODO
+    * @param supplementVo
+    * @return
+    * @throws Exception
+    * @return int
+     */
+    int updateReportStts(SupplementVo supplementVo) throws Exception;
+    
+    /**
+    * 컨설팅 정보 조회. 
+    *
+    * @Title : selectConsulting
+    * @Description : TODO
+    * @param reportVo
+    * @return
+    * @throws Exception
+    * @return ReportVo
+     */
+    ReportVo selectConsulting(ReportVo reportVo) throws Exception;
+}
