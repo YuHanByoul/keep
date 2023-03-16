c5d7ea87fe9b7d2682690b63a847443e9c443523
원종민 <wjm358@kbrainc.com>
2023-03-16 16:43:16 +0900

    헬프데스크 마스킹 및 반응형 처리

--- src/main/java/com/kbrainc/plum/front/helpdesk/model/HelpdeskAnsVo.java
+++ src/main/java/com/kbrainc/plum/front/helpdesk/model/HelpdeskAnsVo.java
@@ -1,6 +1,7 @@
 package com.kbrainc.plum.front.helpdesk.model;
 
 import com.kbrainc.plum.cmm.file.model.FileVo;
+import com.kbrainc.plum.rte.util.StringUtil;
 import lombok.Data;
 import org.apache.ibatis.type.Alias;
 
@@ -24,13 +25,34 @@ import java.util.List;
 @Alias("front.HelpdeskAnsVo")
 @Data
 public class HelpdeskAnsVo {
+    /** 답변 아이디 */
     private Integer ansid;
+
+    /** 답변 날짜 */
     private String ansDe;
+
+    /** 제목 */
     private String ttl;
+
+    /** 내용 */
     private String cn;
+
+    /** 답변자 아이디 */
     private String acnt;
+
+    /** 답변자 이름 */
+    private String nm;
+
+    /** 파일 그룹 아이디 */
     private Integer filegrpid;
+
+    /** 공개 여부 */
     private String rlsYn;
+
+    /** 파일 목록 */
     private List<FileVo> fileList;
 
+    public void setNm(String nm) {
+        this.nm = StringUtil.maskingName(nm);
+    }
 }
