4334364c6229907c57f6a5b985308fc6a2d3ffeb
원종민 <wjm358@kbrainc.com>
2023-03-16 17:34:26 +0900

    플랫폼 개선 제안 마스킹 및 반응형 처리

--- src/main/java/com/kbrainc/plum/front/pltfomImprvmPropsl/serivce/PltfomImprvmPropslService.java
+++ src/main/java/com/kbrainc/plum/front/pltfomImprvmPropsl/serivce/PltfomImprvmPropslService.java
@@ -22,19 +22,91 @@ import java.util.Map;
  * @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
  */
 public interface PltfomImprvmPropslService {
+    /**
+     * 플랫폼 개선 제안 생성
+     *
+     * @param pltfomImprvmPropslVo
+     * @return int
+     * @throws Exception
+     * @Title : insertPltfomImprvmPropsl
+     * @Description : 플랫폼 개선 제안 생성
+     */
     public int insertPltfomImprvmPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 수정
+     *
+     * @param pltfomImprvmPropslVo
+     * @return int
+     * @throws Exception
+     * @Title : updatePltfomImprvmPropsl
+     * @Description : 플랫폼 개선 제안 수정
+     */
     public int updatePltfomImprvmPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 삭제
+     *
+     * @param pltfomImprvmPropslVo
+     * @return int
+     * @throws Exception
+     * @Title : deletePltfomImprvmPropsl
+     * @Description : 플랫폼 개선 제안 삭제
+     */
     public int deletePltfomImprvmPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 취소
+     *
+     * @param pltfomImprvmPropslVo
+     * @return int
+     * @throws Exception
+     * @Title : cancelPltfomImprvmPropsl
+     * @Description : 플랫폼 개선 제안 취소
+     */
     public int cancelPltfomImprvmPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 목록 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return list
+     * @throws Exception
+     * @Title : selectPltfomImprvmPropslList
+     * @Description : 플랫폼 개선 제안 목록 조회
+     */
     public List<PltfomImprvmPropslVo> selectPltfomImprvmPropslList(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 마이페이지 > 플랫폼 개선 제안 목록 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return list
+     * @throws Exception
+     * @Title : selectMyPltfomImprvmPropslList
+     * @Description : 마이페이지 > 플랫폼 개선 제안 목록 조회
+     */
     public List<PltfomImprvmPropslVo> selectMyPltfomImprvmPropslList(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 상세 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return pltfom imprvm propsl vo
+     * @throws Exception
+     * @Title : selectPropsl
+     * @Description : 플랫폼 개선 제안 상세 조회
+     */
     public PltfomImprvmPropslVo selectPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 
+    /**
+     * 플랫폼 개선 제안 답변 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return pltfom imprvm propsl ans vo
+     * @throws Exception
+     * @Title : selectPropslAns
+     * @Description : 플랫폼 개선 제안 답변 조회
+     */
     public PltfomImprvmPropslAnsVo selectPropslAns(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
 }
