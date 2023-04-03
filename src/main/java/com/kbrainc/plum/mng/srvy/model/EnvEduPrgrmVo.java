1f3232f959a747f1ff7cbead1db237f1785ac6df
박희열 <phy@kbrainc.com>
2023-04-03 13:47:39 +0900

    - 관리자 - 설문관리 유아환경교육/푸름이아동환경교실 추가

---
+++ src/main/java/com/kbrainc/plum/mng/srvy/model/EnvEduPrgrmVo.java
@@ -0,0 +1,90 @@
+package com.kbrainc.plum.mng.srvy.model;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kbrainc.plum.rte.model.ParentRequestVo;
+import com.kbrainc.plum.rte.model.UserVo;
+
+import lombok.Data;
+
+/**
+ * 
+ * 유아환경교육설문 대상프로그램 Vo 클래스
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.srvy.model
+ * - EnvEduPrgrmVo.java
+ * </pre> 
+ *
+ * @ClassName : EnvEduPrgrmVo
+ * @Description : 유아환경교육설문 대상프로그램 Vo 클래스 
+ * @author : KBRAINC
+ * @date : 2023. 03. 31.
+ * @Version : 
+ * @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+ */
+@Data
+public class EnvEduPrgrmVo extends ParentRequestVo {
+    
+    /** 로그인 사용자 정보 */
+    private UserVo user;
+    
+    /** 프로그램명 */
+    private String prgrmNm;
+    
+    /** 교육관명 */
+    private String clssrmNm;
+    
+    /** 교육연도 */
+    private int eduYr;
+    
+    /** 수정일시 */
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
+    private Date mdfcnDt;
+    
+    /** 수정자 아이디 */
+    private String mdfrid;
+    
+    /** 등록일시 */
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
+    private Date regDt;
+    
+    /** 등록자 아이디 */
+    private String rgtrid;
+    
+    /** 수정일자 정보 */
+    public Date getMdfcnDt() {
+        return mdfcnDt != null ? (Date) mdfcnDt.clone() : null;
+    }
+    
+    public void setMdfcnDt(Date mdfcnDt) {
+        this.mdfcnDt = mdfcnDt != null ? (Date) mdfcnDt.clone() : null;
+    }
+    
+    /** 등록일자 정보 */
+    public Date getRegDt() {
+        return regDt != null ? (Date) regDt.clone() : null;
+    }
+    
+    public void setRegDt(Date regDt) {
+        this.regDt = regDt != null ? (Date) regDt.clone() : null;
+    }
+    
+    /** 로그인 사용자 정보 */
+    public UserVo getUser() {
+        UserVo clone = (UserVo) SerializationUtils.clone(this.user);
+        return  clone;
+    }
+    
+    public void setUser(UserVo user) {
+        UserVo clone = (UserVo) SerializationUtils.clone(user);
+        this.user = clone;
+    }
+    
+}
