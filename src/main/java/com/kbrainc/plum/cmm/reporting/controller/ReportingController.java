da4b69a4347285122da415d52ec9695f80b61e1b
서정도 <sjd4328@kbrainc.com>
2023-04-03 15:02:52 +0900

    [공통]레포팅 파일명, 컨트롤러 코드 수정

--- src/main/java/com/kbrainc/plum/cmm/reporting/controller/ReportingController.java
+++ src/main/java/com/kbrainc/plum/cmm/reporting/controller/ReportingController.java
@@ -8,10 +8,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ReportingController {
     
-    @RequestMapping("/cmm/reporting/reportDetail.html")
-    public String reportDetail(@RequestParam String fileName, @RequestParam String id, Model model) throws Exception {
+    @RequestMapping("/cmm/reporting/dsgnPrgrmOutl.html")
+    public String dsgnPrgrmOutl(@RequestParam String id, Model model) throws Exception {
         model.addAttribute("id", id);
         
-        return "cmm/reporting/"+fileName+".html";
+        return "cmm/reporting/dsgnPrgrmOutl.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/dsgnPrgrmDetail.html")
+    public String dsgnPrgrmDetail(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/dsgnPrgrmDetail.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/cnsltngResult.html")
+    public String cnsltngResult(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/cnsltngResult.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/prgrmDetail.html")
+    public String prgrmDetail(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/prgrmDetail.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/pcntstDelvryAply.html")
+    public String pcntstDelvryAply(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/pcntstDelvryAply.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/cnsltngResult_online.html")
+    public String cnsltngResult_online(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/cnsltngResult_online.html";
+    }
+    
+    @RequestMapping("/cmm/reporting/cnsltngResult_offline.html")
+    public String reportDetail(@RequestParam String id, Model model) throws Exception {
+        model.addAttribute("id", id);
+        
+        return "cmm/reporting/cnsltngResult_offline.html";
     }
 }
