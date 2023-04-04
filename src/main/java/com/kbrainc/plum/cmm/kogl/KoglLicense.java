0af11b8395c7f5b24356fe450734096f39fe050e
정승규 <jeongsk@kbrainc.com>
2023-04-04 17:32:56 +0900

    공공누리 저작권 내용 및 이미지경로 수정

--- src/main/java/com/kbrainc/plum/cmm/kogl/KoglLicense.java
+++ src/main/java/com/kbrainc/plum/cmm/kogl/KoglLicense.java
@@ -33,10 +33,10 @@ public class KoglLicense {
         */
         KoglLicense() {
             //info.put("241101", new LicenseTypeVo("241101", "적용안함 (저작권법 제24조 1항 1호~4호의 경우)", null, null, null));
-            info.put("241102", new LicenseTypeVo("241102", "공공누리 1유형 : 출처표시", "본 저작물은 \"공공누리\" 제1유형:출처표시 조건에 따라 이용 할 수 있습니다.", "/images/common/kogl/type1.png", "https://www.kogl.or.kr/info/licenseType1.do"));
-            info.put("241103", new LicenseTypeVo("241103", "공공누리 2유형 : 출처표시, 상업적이용금지", "본 저작물은 \"공공누리\" 제2유형:출처표시+상업적 이용금지 조건에 따라 이용 할 수 있습니다.", "/images/common/kogl/type2.png", "https://www.kogl.or.kr/info/licenseType2.do"));
-            info.put("241104", new LicenseTypeVo("241104", "공공누리 3유형 : 출처표시, 변경금지", "본 저작물은 \"공공누리\" 제3유형:출처표시+변경금지 조건에 따라 이용 할 수 있습니다.", "/images/common/kogl/type3.png", "https://www.kogl.or.kr/info/licenseType3.do"));
-            info.put("241105", new LicenseTypeVo("241105", "공공누리 4유형 : 출처표시, 상업적이용금지, 변경금지", "본 저작물은 \"공공누리\" 제4유형:출처표시+상업적 이용금지+변경금지 조건에 따라 이용 할 수 있습니다.", "/images/common/kogl/type4.png", "https://www.kogl.or.kr/info/licenseType4.do"));
+            info.put("241102", new LicenseTypeVo("241102", "공공누리 1유형 : 출처표시", "본 저작물은 “공공누리” <br class=\"mo-only\"> 제1유형:출처표시 <br class=\"mo-only\"> 조건에 따라 이용할 수 있습니다.", "/images/front/opentype/opentype01.jpg", "https://www.kogl.or.kr/info/licenseType1.do"));
+            info.put("241103", new LicenseTypeVo("241103", "공공누리 2유형 : 출처표시, 상업적이용금지", "본 저작물은 “공공누리” <br class=\"mo-only\"> 제2유형:출처표시+상업적 이용금지 <br class=\"mo-only\"> 조건에 따라 이용할 수 있습니다.", "/images/front/opentype/opentype02.jpg", "https://www.kogl.or.kr/info/licenseType2.do"));
+            info.put("241104", new LicenseTypeVo("241104", "공공누리 3유형 : 출처표시, 변경금지", "본 저작물은 “공공누리” <br class=\"mo-only\"> 제3유형:출처표시+변경금지 <br class=\"mo-only\"> 조건에 따라 이용할 수 있습니다.", "/images/front/opentype/opentype03.jpg", "https://www.kogl.or.kr/info/licenseType3.do"));
+            info.put("241105", new LicenseTypeVo("241105", "공공누리 4유형 : 출처표시, 상업적이용금지, 변경금지", "본 저작물은 “공공누리” <br class=\"mo-only\"> 제4유형:출처표시+상업적 이용금지+변경금지 <br class=\"mo-only\"> 조건에 따라 이용할 수 있습니다.", "/images/front/opentype/opentype04.jpg", "https://www.kogl.or.kr/info/licenseType4.do"));
         }
     
         /**
