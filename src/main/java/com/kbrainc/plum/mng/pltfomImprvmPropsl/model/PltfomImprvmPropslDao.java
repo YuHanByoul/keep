5524e82ce83b9d23d6e0a8eea39d29c9efc0b953
원종민 <wjm358@kbrainc.com>
2023-04-13 16:58:32 +0900

    [관리자] 알림문의 > 고객센터 > 플랫폼개선제안 관리 중간 커밋

---
+++ src/main/java/com/kbrainc/plum/mng/pltfomImprvmPropsl/model/PltfomImprvmPropslDao.java
@@ -0,0 +1,90 @@
+package com.kbrainc.plum.mng.pltfomImprvmPropsl.model;
+
+import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
+
+import java.util.List;
+
+/**
+ * 알림/문의 > 고객센터 > 플랫폼개선제안 Dao 인터페이스
+ *
+ * <pre>
+ * com.kbrainc.plum.mng.pltfomImprvmPropsl.model
+ * - PltfomImprvmPropslDao.java
+ * </pre>
+ *
+ * @author : KBRAINC_DEV
+ * @ClassName : PltfomImprvmPropslDao
+ * @Description : 알림/문의 > 고객센터 > 플랫폼개선제안 Dao 인터페이스
+ * @date : 2023. 04. 13.
+ * @Version :
+ * @Company : CopyrightⒸ KBRAIN Company. All Rights Reserved
+ */
+@Mapper
+public interface PltfomImprvmPropslDao {
+    /**
+     * 플랫폼개선제안 목록 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return list
+     * @throws Exception
+     * @Title : selectPltfomImprvmPropslList
+     * @Description : 플랫폼 개선 제안 목록 조회
+     */
+    public List<PltfomImprvmPropslVo> selectPltfomImprvmPropslList(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
+
+    /**
+     * 플랫폼개선제안 문의 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return pltfom imprvm propsl vo
+     * @throws Exception
+     * @Title : selectPltfomImprvmPropsl
+     * @Description : 플랫폼개선제안 문의 조회
+     */
+    public PltfomImprvmPropslVo selectPltfomImprvmPropsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
+
+    /**
+     * 플랫폼개선제안 답변 조회
+     *
+     * @param pltfomImprvmPropslVo
+     * @return pltfom imprvm propsl ans vo
+     * @throws Exception
+     * @Title : selectPltfomImprvmPropslAns
+     * @Description : 플랫폼개선제안 답변 조회
+     */
+    public PltfomImprvmPropslAnsVo selectPltfomImprvmPropslAns(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
+
+    /**
+     * 플랫폼개선제안 답변 등록
+     *
+     * @param pltfomImprvmPropslAnsVo
+     * @return int
+     * @throws Exception
+     * @Title : insertPltfomImprvmPropslAns
+     * @Description : 플랫폼개선제안 답변 등록
+     */
+    public int insertPltfomImprvmPropslAns(PltfomImprvmPropslAnsVo pltfomImprvmPropslAnsVo) throws Exception;
+
+    /**
+     * 플랫폼개선제안 답변 수정
+     *
+     * @param pltfomImprvmPropslAnsVo
+     * @return int
+     * @throws Exception
+     * @Title : updatePltfomImprvmPropslAns
+     * @Description : 플랫폼개선제안 답변 수정
+     */
+    public int updatePltfomImprvmPropslAns(PltfomImprvmPropslAnsVo pltfomImprvmPropslAnsVo) throws Exception;
+
+    /**
+     * 플랫폼개선제안 삭제
+     *
+     * @param pltfomImprvmPropslVo
+     * @return int
+     * @throws Exception
+     * @Title : deletePltfomImprvmPrpsl
+     * @Description : 플랫폼개선제안 삭제
+     */
+    public int deletePltfomImprvmPrpsl(PltfomImprvmPropslVo pltfomImprvmPropslVo) throws Exception;
+
+}
